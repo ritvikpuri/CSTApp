e94ccc8205f1b704b41048973086425c52010fbf
Alexandra <alexandra.tran@outlook.com>
2019-03-23 20:06:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cstapp/IndividualActivity.java
@@ -0,0 +1,13 @@
+package com.example.cstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndividualActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual);
+    }
+}
